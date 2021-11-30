7a04b9666e1c02221e4c1e56a6828846109500f0
jessica-luo <jessicaluoaustin@gmail.com>
2021-11-30 03:10:45 -0500

    A9

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/webdev');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
@@ -20,5 +22,6 @@ app.get('/hello', (req, res) => {
 
 require('./services/movies-service')(app);
 require('./services/tweeter-service')(app);
+require('./movies/service')(app);
 
-app.listen(process.env.PORT || 4000);
+app.listen(4000 || process.env.PORT);
--- services/tweeter-service.js
+++ services/tweeter-service.js
@@ -1,57 +1,24 @@
-let tweets = require('../data/tweets.json');
+const dao = require('../db/tweets/tweet-dao');
 
 module.exports = (app) => {
 
     const findAllTweets = (req, res) => {
-        res.json(tweets);
+        dao.findAllTweets()
+            .then(tweets => res.json(tweets));
     }
 
     const createTweet = (req, res) => {
-        const newTweet = {
-            _id: (new Date()).getTime() + '',
-            "topic": "Web Development",
-            "userName": "ReactJS",
-            "verified": false,
-            "handle": "ReactJS",
-            "time": "2h",
-            "avatar-image": "/images/nodejs.png",
-            "logo-image": "/images/nodejs.png",
-            "stats": {
-                "comments": 0,
-                "retweets": 0,
-                "likes": 0
-            },
-            ...req.body,
-        }
-        tweets = [
-            newTweet,
-            ...tweets
-        ];
-        res.json(newTweet);
+        dao.createTweet(req.body)
+            .then((insertedTweet) => res.json(insertedTweet));
     }
 
     const deleteTweet = (req, res) => {
-        const id = req.params['id'];
-        tweets = tweets.filter(tweet => tweet._id !== id);
-        res.sendStatus(200);
+        dao.deleteTweet(req.params.id)
+            .then((status) => res.send(status));
     }
     const likeTweet = (req, res) => {
-        const id = req.params['id'];
-        tweets = tweets.map(tweet => {
-            if (tweet._id === id) {
-                if (tweet.liked === true) {
-                    tweet.liked = false;
-                    tweet.stats.likes--;
-                } else {
-                    tweet.liked = true;
-                    tweet.stats.likes++;
-                }
-                return tweet;
-            } else {
-                return tweet;
-            }
-        });
-        res.sendStatus(200);
+        dao.updateTweet(req.params.id, req.body)
+            .then(status => res.send(status));
     }
 
     app.put('/api/tweets/:id/like', likeTweet);
